d023590a034f5f3d93e706d6e7500dd2e01df480
rvosistemas <darkrdz@gmail.com>
2019-07-21 02:29:07 -0500

    se agrego el rining y los colores a las texturas, falta exportarlo tipo obj

---
+++ scripts/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public Animator Anim;
+    public float WalkSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ( Input.GetKey( KeyCode.W ) || Input.GetKey( KeyCode.UpArrow ) )
+        {
+
+            Anim.SetBool( "Walk", true );
+            transform.Translate( Vector3.forward * WalkSpeed * Time.deltaTime );
+
+        }
+        else {
+
+            Anim.SetBool( "Walk", false );
+
+        }
+
+	}
+}
